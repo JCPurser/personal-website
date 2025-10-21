9537d653965ed259b43ecec94abd26608a95db8f
JCPurser <james@cpurser.co.uk>
2025-10-21 20:48:54 +0100

    Confirming Linking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Check to see if the website is working.
           </li>
         </ol>
 
